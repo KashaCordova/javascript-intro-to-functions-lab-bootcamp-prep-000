6edb523fb3a51fe695365463b2bfdf23658aa98a
Celia Gearin <cgearin18@gmail.com>
2018-05-05 08:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function shout(string) { return string.toUpperCase();
 }
-function whisper(string) {return string.toLowerCase();
+function whisper(string) { return string.toLowerCase();
+}                                            
+function logshout(string) { console.log(string.toUpperCase);
 }
